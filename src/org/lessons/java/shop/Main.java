371ec8761725575c63e54fdd1272c2d9255c7931
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-06 14:23:48 +0100

    create the products

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,4 +1,12 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Main {
+    public static void main(String[] args) {
+        Random generateRandom = new Random();
+        int generateCode = generateRandom.nextInt(0, 1001);
+        Prodotto prodotto1 = new Prodotto(generateCode, "Cellulare", "Iphone 14", 560.99, 50.00);
+
+    }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -7,4 +7,66 @@ public class Prodotto {
     private String description;
     private double price;
     private double iva;
+
+    //COSTRUTTORI
+
+    public Prodotto(int code, String name, String description, double price, double iva) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
+
+
+    //GETTER E SETTER
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+
+    //METODI
+
+    public void price() {
+         System.out.println("Il prezzo base del prodotto è di " + price + "$");
+         System.out.println("Il prezzo totale compreso di iva è di " + priceWithIva() + "$");
+    }
+
+    private double priceWithIva() {
+        double totalPrice = price + iva;
+        return totalPrice;
+    }
 }
