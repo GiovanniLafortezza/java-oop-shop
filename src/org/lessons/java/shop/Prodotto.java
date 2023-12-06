22d6b7064335f389464fbf43dd7bf4c436bdbae2
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-06 13:57:02 +0100

    create attributes

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,4 +1,10 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+    //ATTRIBUTI
+    private int code;
+    private String name;
+    private String description;
+    private double price;
+    private double iva;
 }
